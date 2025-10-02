2a4e7d63695cef88863f937b5077231b221de1e8
CHALCOGEN645 <166417113+CHALCOGEN645@users.noreply.github.com>
2025-10-02 13:35:10 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,9 +31,6 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/Chalcogen645/xbit.git")],
-    
-    ]
+        ]
     
     return buttons
