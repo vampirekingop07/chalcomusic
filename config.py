4ecd1ec8b8d04611c549bb73155c58dce5b740af
vampirekingop07 <naaztravels62@gmail.com>
2025-10-23 16:00:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY =  getenv("API_KEY", 'xbit_GD1913XU45B4EHDKIUBXRY') ## Your API key like: xbit_GD1913XU45B4EHDKIUBXRY Get from  https://t.me/tgmusic_apibot
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://pytdbotapi.thequickearn.xyz') ## xBit Music Endpoint.
+YT_API_KEY =  getenv("API_KEY", 'NxGBNexGenBots2d8c91') ## Your API key like: NxGBNexGenBots2d8c91 Get from  https://pytdbotapi.thequickearn.xyz
 
 ## Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
